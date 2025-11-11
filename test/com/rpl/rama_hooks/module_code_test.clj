b2ec1103843428140319796b2fe996b25c103b59
Hugo Duncan <hugo@hugoduncan.org>
2025-11-11 08:02:58 -0500

    feat: add cljfmt code formatter with pre-commit hook
    Add cljfmt to the project with proper configuration, pre-commit hook integration, and perform initial formatting.
    
    - Add dev.weavejester/cljfmt 0.15.3 as :cljfmt alias
    - Create .cljfmt.edn with indentation rules for with-testing-context
    - Add install-pre-commit-hook.sh script to auto-format on commit
    - Reformat all test files using cljfmt

--- test/com/rpl/rama_hooks/module_code_test.clj
+++ test/com/rpl/rama_hooks/module_code_test.clj
@@ -1,182 +1,182 @@
 (ns com.rpl.rama-hooks.module-code-test
-  (:require
-   [clj-kondo.hooks-api :as api]
-   [clojure.test :refer [deftest is testing]]
-   [com.rpl.errors :as err]
-   [com.rpl.rama-hooks :as rama]
-   [com.rpl.test-helpers :refer [get-error-messages
-                                  sexpr->node
-                                  node->sexpr
-                                  transform-module-sexprs
-                                  ->sexpr
-                                  with-testing-context]]))
+    (:require
+     [clj-kondo.hooks-api :as api]
+     [clojure.test :refer [deftest is testing]]
+     [com.rpl.errors :as err]
+     [com.rpl.rama-hooks :as rama]
+     [com.rpl.test-helpers :refer [get-error-messages
+                                   sexpr->node
+                                   node->sexpr
+                                   transform-module-sexprs
+                                   ->sexpr
+                                   with-testing-context]]))
 
 (deftest module-hook-test
   ;; Test module-hook transformation of module code (declare-depot, declare-pstate)
   ;; Contracts: transforms linear module forms into nested let expressions
   (testing "module-hook"
-    (testing "Basic expressions within a module"
-      (is (= '(let
-               [*depot (declare-depot setup (hash-by :x))]
-               (pr *depot))
-             (api/sexpr
-              (first
-               (rama/transform-module-form
-                (->sexpr '(declare-depot setup *depot (hash-by :x)))
-                nil
-                #{})))))
-      (is
-       (= '(let
-            [*depot (declare-depot setup (hash-by :x))]
-            (pr *depot)
-            (let
-             [*depot-2 (declare-depot setup (hash-by :y))]
-             (pr *depot-2)
-             (pr *depot-2)))
-          (api/sexpr (first
-                      (transform-module-sexprs
-                       '(declare-depot setup *depot (hash-by :x))
-                       '(declare-depot setup *depot-2 (hash-by :y))
-                       '(pr *depot-2)))))))
+           (testing "Basic expressions within a module"
+                    (is (= '(let
+                             [*depot (declare-depot setup (hash-by :x))]
+                             (pr *depot))
+                           (api/sexpr
+                            (first
+                             (rama/transform-module-form
+                              (->sexpr '(declare-depot setup *depot (hash-by :x)))
+                              nil
+                              #{})))))
+                    (is
+                     (= '(let
+                          [*depot (declare-depot setup (hash-by :x))]
+                          (pr *depot)
+                          (let
+                           [*depot-2 (declare-depot setup (hash-by :y))]
+                           (pr *depot-2)
+                           (pr *depot-2)))
+                        (api/sexpr (first
+                                    (transform-module-sexprs
+                                     '(declare-depot setup *depot (hash-by :x))
+                                     '(declare-depot setup *depot-2 (hash-by :y))
+                                     '(pr *depot-2)))))))
 
-    (testing "Basic anonymous module definitions"
-      (let [module-def
-            '(module
-              [setup topologies]
-              (declare-depot setup *transfer-depot (hash-by :x))
-              (let
-               [mb (microbatch-topology topologies "banking")]
-               (declare-pstate mb $$funds {Long Long})))]
-        (is
-         (=
-          '(fn
-            [setup topologies]
-            (let
-             [*transfer-depot (declare-depot setup (hash-by :x))]
-             (pr *transfer-depot)
-             (let
-              [mb (microbatch-topology topologies "banking")]
-              (let
-               [$$funds (declare-pstate mb {Long Long})]
-               (pr $$funds)))))
-          (node->sexpr
-           (rama/module-hook
-            (sexpr->node module-def)))))))
+           (testing "Basic anonymous module definitions"
+                    (let [module-def
+                          '(module
+                            [setup topologies]
+                            (declare-depot setup *transfer-depot (hash-by :x))
+                            (let
+                             [mb (microbatch-topology topologies "banking")]
+                             (declare-pstate mb $$funds {Long Long})))]
+                         (is
+                          (=
+                           '(fn
+                             [setup topologies]
+                             (let
+                              [*transfer-depot (declare-depot setup (hash-by :x))]
+                              (pr *transfer-depot)
+                              (let
+                               [mb (microbatch-topology topologies "banking")]
+                               (let
+                                [$$funds (declare-pstate mb {Long Long})]
+                                (pr $$funds)))))
+                           (node->sexpr
+                            (rama/module-hook
+                             (sexpr->node module-def)))))))
 
-    (with-testing-context
-     "Errors if the module input vectors is missing, or isn't a vector"
-     (rama/module-hook
-      (sexpr->node '(module)))
+           (with-testing-context
+            "Errors if the module input vectors is missing, or isn't a vector"
+             (rama/module-hook
+              (sexpr->node '(module)))
 
-     (rama/module-hook
-      (sexpr->node '(module ())))
+             (rama/module-hook
+              (sexpr->node '(module ())))
 
-     (rama/module-hook
-      (sexpr->node '(module {:module-name "xyz"} ())))
+             (rama/module-hook
+              (sexpr->node '(module {:module-name "xyz"} ())))
 
-     (rama/module-hook
-      (sexpr->node '(module {:module-name "xyz"})))
+             (rama/module-hook
+              (sexpr->node '(module {:module-name "xyz"})))
 
-     (is (= [err/syntax-error-missing-input-vector
-             err/syntax-error-missing-input-vector
-             err/syntax-error-missing-input-vector
-             err/syntax-error-missing-input-vector]
-            (get-error-messages))))))
+             (is (= [err/syntax-error-missing-input-vector
+                     err/syntax-error-missing-input-vector
+                     err/syntax-error-missing-input-vector
+                     err/syntax-error-missing-input-vector]
+                    (get-error-messages))))))
 
 (deftest defmodule-hook-test
   ;; Test defmodule-hook transformation of module definitions
   ;; Contracts: transforms defmodule into defn with nested let expressions
   (testing "defmodule-hook"
-    (testing "Basic defmodule definitions"
-      (let [module-def
-            '(defmodule
-              MyModule
-              [setup topologies]
-              (declare-depot setup *transfer-depot (hash-by :x))
-              (let
-               [mb (microbatch-topology topologies "banking")]
-               (declare-pstate mb $$funds {Long Long})))]
-        (is
-         (=
-          '(defn
-            MyModule
-            [setup topologies]
-            (let
-             [*transfer-depot (declare-depot setup (hash-by :x))]
-             (pr *transfer-depot)
-             (let
-              [mb (microbatch-topology topologies "banking")]
-              (let
-               [$$funds (declare-pstate mb {Long Long})]
-               (pr $$funds)))))
-          (node->sexpr
-           (rama/defmodule-hook
-            (sexpr->node module-def)))))))
+           (testing "Basic defmodule definitions"
+                    (let [module-def
+                          '(defmodule
+                            MyModule
+                            [setup topologies]
+                            (declare-depot setup *transfer-depot (hash-by :x))
+                            (let
+                             [mb (microbatch-topology topologies "banking")]
+                             (declare-pstate mb $$funds {Long Long})))]
+                         (is
+                          (=
+                           '(defn
+                             MyModule
+                             [setup topologies]
+                             (let
+                              [*transfer-depot (declare-depot setup (hash-by :x))]
+                              (pr *transfer-depot)
+                              (let
+                               [mb (microbatch-topology topologies "banking")]
+                               (let
+                                [$$funds (declare-pstate mb {Long Long})]
+                                (pr $$funds)))))
+                           (node->sexpr
+                            (rama/defmodule-hook
+                             (sexpr->node module-def)))))))
 
-    (testing "Full defmodule definitions"
-      (is
-       (=
-        '(defn
-          RestAPIIntegrationModule
-          [setup topologies]
-          (let
-           [*depot (declare-depot setup (hash-by identity))]
-           (pr *depot)
-           (let
-            [*depot-2 (declare-depot setup (hash-by identity))]
-            (pr *depot-2)
-            (let
-             [s (stream-topology topologies "s")]
-             (let
-              [$$p1 (declare-pstate s {Object Object})]
-              (pr $$p1)
-              (let
-               [$$p2 (declare-pstate s {Object Long})]
-               (pr $$p2)
-               (fn
-                []
-                (let
-                 [[*k *v] (source> *depot)]
-                 (+compound $$p {*k (+vec-agg *v)}))
-                (let
-                 [*key (source> *depot-2)]
-                 (+compound $$p {*key (+count)})))))))))
-        (node->sexpr
-         (rama/defmodule-hook
-          (sexpr->node '(defmodule
-                         RestAPIIntegrationModule
-                         [setup topologies]
-                         (declare-depot setup *depot (hash-by identity))
-                         (declare-depot setup *depot-2 (hash-by identity))
+           (testing "Full defmodule definitions"
+                    (is
+                     (=
+                      '(defn
+                        RestAPIIntegrationModule
+                        [setup topologies]
+                        (let
+                         [*depot (declare-depot setup (hash-by identity))]
+                         (pr *depot)
                          (let
-                          [s (stream-topology topologies "s")]
-                          (declare-pstate s $$p1 {Object Object})
-                          (declare-pstate s $$p2 {Object Long})
-                          (<<sources
-                           s
-                          (source> *depot :> [*k *v])
-                           (+compound $$p {*k (+vec-agg *v)})
-                          (source> *depot-2 :> *key)
-                           (+compound $$p {*key (+count)}))))))))))
+                          [*depot-2 (declare-depot setup (hash-by identity))]
+                          (pr *depot-2)
+                          (let
+                           [s (stream-topology topologies "s")]
+                           (let
+                            [$$p1 (declare-pstate s {Object Object})]
+                            (pr $$p1)
+                            (let
+                             [$$p2 (declare-pstate s {Object Long})]
+                             (pr $$p2)
+                             (fn
+                              []
+                              (let
+                               [[*k *v] (source> *depot)]
+                               (+compound $$p {*k (+vec-agg *v)}))
+                              (let
+                               [*key (source> *depot-2)]
+                               (+compound $$p {*key (+count)})))))))))
+                      (node->sexpr
+                       (rama/defmodule-hook
+                        (sexpr->node '(defmodule
+                                       RestAPIIntegrationModule
+                                       [setup topologies]
+                                       (declare-depot setup *depot (hash-by identity))
+                                       (declare-depot setup *depot-2 (hash-by identity))
+                                       (let
+                                        [s (stream-topology topologies "s")]
+                                        (declare-pstate s $$p1 {Object Object})
+                                        (declare-pstate s $$p2 {Object Long})
+                                        (<<sources
+                                         s
+                                         (source> *depot :> [*k *v])
+                                         (+compound $$p {*k (+vec-agg *v)})
+                                         (source> *depot-2 :> *key)
+                                         (+compound $$p {*key (+count)}))))))))))
 
-    (with-testing-context
-     "Errors if the module name or input vectors are missing, or don't have the correct type"
-     (rama/defmodule-hook
-      (sexpr->node '(defmodule)))
+           (with-testing-context
+            "Errors if the module name or input vectors are missing, or don't have the correct type"
+             (rama/defmodule-hook
+              (sexpr->node '(defmodule)))
 
-     (rama/defmodule-hook
-      (sexpr->node '(defmodule 123)))
+             (rama/defmodule-hook
+              (sexpr->node '(defmodule 123)))
 
-     (rama/defmodule-hook
-      (sexpr->node '(defmodule 123 [])))
+             (rama/defmodule-hook
+              (sexpr->node '(defmodule 123 [])))
 
-     (rama/defmodule-hook
-      (sexpr->node '(defmodule symbol (xyz))))
+             (rama/defmodule-hook
+              (sexpr->node '(defmodule symbol (xyz))))
 
-     (is (= [err/syntax-error-missing-def-name
-             err/syntax-error-missing-input-vector
-             err/syntax-error-missing-def-name
-             err/syntax-error-missing-input-vector
-             err/syntax-error-missing-def-name
-             err/syntax-error-missing-input-vector]
-            (get-error-messages))))))
+             (is (= [err/syntax-error-missing-def-name
+                     err/syntax-error-missing-input-vector
+                     err/syntax-error-missing-def-name
+                     err/syntax-error-missing-input-vector
+                     err/syntax-error-missing-def-name
+                     err/syntax-error-missing-input-vector]
+                    (get-error-messages))))))
--- test/com/rpl/test_helpers.clj
+++ test/com/rpl/test_helpers.clj
@@ -1,74 +1,74 @@
 (ns com.rpl.test-helpers
-  "Shared test utilities for rama-clj-kondo tests."
-  (:require
-   [clj-kondo.hooks-api :as api]
-   [clj-kondo.impl.utils :as utils :refer [parse-string]]
-   [clojure.test :refer [testing]]
-   [com.rpl.rama-hooks :as rama]))
+    "Shared test utilities for rama-clj-kondo tests."
+    (:require
+     [clj-kondo.hooks-api :as api]
+     [clj-kondo.impl.utils :as utils :refer [parse-string]]
+     [clojure.test :refer [testing]]
+     [com.rpl.rama-hooks :as rama]))
 
 (defn get-first-error-message
-  "Returns the message from the first finding in the current test context."
-  []
-  (-> utils/*ctx*
-      :findings
-      deref
-      first
-      :message))
+      "Returns the message from the first finding in the current test context."
+      []
+      (-> utils/*ctx*
+          :findings
+          deref
+          first
+          :message))
 
 (defn get-error-messages
-  "Returns a vector of all finding messages in the current test context."
-  []
-  (->> utils/*ctx*
-       :findings
-       deref
-       (mapv :message)))
+      "Returns a vector of all finding messages in the current test context."
+      []
+      (->> utils/*ctx*
+           :findings
+           deref
+           (mapv :message)))
 
 (defmacro with-testing-context
-  "Creates a testing context with isolated clj-kondo state for findings.
+          "Creates a testing context with isolated clj-kondo state for findings.
   Use this to wrap test assertions that check for linting errors."
-  [test-desc & body]
-  `(testing ~test-desc
-     (binding [utils/*ctx* {:base-lang  :clj
-                            :lang       :clj
-                            :namespaces (atom {:clj {:clj {}}})
-                            :findings   (atom [])
-                            :ignores    (atom {})}]
-       ~@body)))
+          [test-desc & body]
+          `(testing ~test-desc
+                    (binding [utils/*ctx* {:base-lang  :clj
+                                           :lang       :clj
+                                           :namespaces (atom {:clj {:clj {}}})
+                                           :findings   (atom [])
+                                           :ignores    (atom {})}]
+                             ~@body)))
 
 (defn ->sexpr
-  "Converts a Clojure expression to a clj-kondo node by printing and parsing."
-  [expr]
-  (parse-string (prn-str expr)))
+      "Converts a Clojure expression to a clj-kondo node by printing and parsing."
+      [expr]
+      (parse-string (prn-str expr)))
 
 (defn transform-sexprs
-  "Transforms multiple s-expressions using rama/transform-body."
-  [& sexprs]
-  (let [sexprs (mapv ->sexpr sexprs)]
-    (rama/transform-body sexprs)))
+      "Transforms multiple s-expressions using rama/transform-body."
+      [& sexprs]
+      (let [sexprs (mapv ->sexpr sexprs)]
+           (rama/transform-body sexprs)))
 
 (defn transform-module-sexprs
-  "Transforms multiple s-expressions as module body using rama/transform-module-body."
-  [& sexprs]
-  (rama/transform-module-body
-   (mapv ->sexpr sexprs)
-   #{}))
+      "Transforms multiple s-expressions as module body using rama/transform-module-body."
+      [& sexprs]
+      (rama/transform-module-body
+       (mapv ->sexpr sexprs)
+       #{}))
 
 (defn strip-trampoline
-  "Removes all instances of 'trampoline symbols from the s-expression."
-  [sexpr]
-  (rama/remove-instances sexpr #{'trampoline}))
+      "Removes all instances of 'trampoline symbols from the s-expression."
+      [sexpr]
+      (rama/remove-instances sexpr #{'trampoline}))
 
 (defn sexpr->node
-  "Wraps an s-expression in a node map for testing."
-  [x]
-  {:node (->sexpr x)})
+      "Wraps an s-expression in a node map for testing."
+      [x]
+      {:node (->sexpr x)})
 
 (defn node->sexpr
-  "Extracts the s-expression from a node map, stripping trampoline symbols."
-  [x]
-  (strip-trampoline (api/sexpr (:node x))))
+      "Extracts the s-expression from a node map, stripping trampoline symbols."
+      [x]
+      (strip-trampoline (api/sexpr (:node x))))
 
 (defn body->sexpr
-  "Extracts the s-expression from the first item in a sequence, stripping trampoline symbols."
-  [x]
-  (strip-trampoline (api/sexpr (first x))))
+      "Extracts the s-expression from the first item in a sequence, stripping trampoline symbols."
+      [x]
+      (strip-trampoline (api/sexpr (first x))))
